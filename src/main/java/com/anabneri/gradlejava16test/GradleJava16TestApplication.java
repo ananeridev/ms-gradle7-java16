4a810e9b53beb6de4824b4f842052beae3eec58a
Ana Beatriz Neri <ananeri32@hotmail.com>
2021-05-19 19:26:16 -0300

    first commit

---
+++ src/main/java/com/anabneri/gradlejava16test/GradleJava16TestApplication.java
@@ -0,0 +1,13 @@
+package com.anabneri.gradlejava16test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleJava16TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleJava16TestApplication.class, args);
+	}
+
+}
